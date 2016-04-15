78450c5e90be01a959468c78cb1964c7b126b0de
Grzesiecki <pawel.grzesiecki@gmail.com>
2016-04-15 20:27:59 +0200

    Updated code coverage

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -158,7 +158,7 @@ class ApiTest extends TestCase
     }
 
     /**
-     * @covers ::testAddApiAuthInfoToParams
+     * @covers ::addApiAuthInfoToParams
      */
     public function testAddApiAuthInfoToParamsWithVersion()
     {
